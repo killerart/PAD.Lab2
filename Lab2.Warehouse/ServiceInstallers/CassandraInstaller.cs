b4ebc3276ea362b00c91271c095b801ae02f5b09
killerart <theemee.work@gmail.com>
2021-12-06 22:59:45 +0200

    added SSL Cassandra connection options

--- Lab2.Warehouse/ServiceInstallers/CassandraInstaller.cs
+++ Lab2.Warehouse/ServiceInstallers/CassandraInstaller.cs
@@ -1,4 +1,8 @@
-﻿using Cassandra;
+﻿using System;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using Cassandra;
 using Cassandra.Mapping;
 using Lab2.Shared.ServiceInstaller;
 using Lab2.Warehouse.Core;
@@ -11,13 +15,29 @@ namespace Lab2.Warehouse.ServiceInstallers {
     public class CassandraInstaller : IServiceInstaller {
         public void AddServices(IServiceCollection services, IConfiguration configuration) {
             MappingConfiguration.Global.Define<IngredientMapping>();
+
+            var options = new SSLOptions(SslProtocols.Tls12, true, ValidateServerCertificate);
+            options.SetHostNameResolver(_ => "pad.cassandra.cosmos.azure.com");
+
             var cassandraConnectionString = configuration.GetConnectionString("Cassandra");
             var cluster = Cluster.Builder()
                                  .WithConnectionString(cassandraConnectionString)
+                                 .WithSSL(options)
                                  .Build();
             var session = cluster.Connect();
             services.AddSingleton<ISession>(session);
             services.AddScoped(typeof(IRepository<>), typeof(CassandraRepository<>));
+        }
+
+        public static bool ValidateServerCertificate(object           sender,
+                                                     X509Certificate? certificate,
+                                                     X509Chain?       chain,
+                                                     SslPolicyErrors  sslPolicyErrors) {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+            return false;
         }
     }
 }
